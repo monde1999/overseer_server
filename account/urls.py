371d571828080c2583abb11fa83d7deb3651db15
monde1999 <52983000+monde1999@users.noreply.github.com>
2021-10-05 15:45:24 +0800

    Added user-info in get-reports()

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,11 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
 urlpatterns = [
+    path('',include(router.urls), name='user_list'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,12 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 @api_view(["POST"])
 def signup(request):
