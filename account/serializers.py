df6818a0a9600833489cefd0dbf05f149adba4cb
monde1999 <52983000+monde1999@users.noreply.github.com>
2021-10-05 16:38:32 +0800

    Added firstname and lastname in report user info

--- account/serializers.py
+++ account/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['id','username']
+        fields = ['id','username','first_name','last_name']
